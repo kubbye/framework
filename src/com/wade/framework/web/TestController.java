b0f9c15835e810da046dc9b391ba2c724a97885f
kubbye <kubbye@163.com>
2014-05-19 20:06:27 +0800

    web

--- src/com/wade/framework/web/TestController.java
+++ src/com/wade/framework/web/TestController.java
@@ -1,5 +1,15 @@
 package com.wade.framework.web;
 
-public class TestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/test")
+public class TestController  {
+
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	
 }
